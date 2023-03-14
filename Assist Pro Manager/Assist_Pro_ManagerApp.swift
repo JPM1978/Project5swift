f6a2b91995a581e4647fb636e12d2d0b02d97398
Jean Mayard <sanctuarygames30@gmail.com>
2023-03-14 16:10:35 -0400

    Initial Commit

---
+++ Assist Pro Manager/Assist_Pro_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assist_Pro_ManagerApp.swift
+//  Assist Pro Manager
+//
+//  Created by Jean on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Assist_Pro_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
